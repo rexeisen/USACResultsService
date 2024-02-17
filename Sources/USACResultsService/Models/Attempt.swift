e9c556efade05795f5f8b281a9678637a132bb08
Jon Rexeisen <jon.rexeisen@gmail.com>
2024-02-17 10:25:13 -0600

    Added attempt structure

---
+++ Sources/USACResultsService/Models/Attempt.swift
@@ -0,0 +1,71 @@
+//
+//  Attempt.swift
+//
+//
+//  Created by Jon Rexeisen on 2/17/24.
+//
+
+import Foundation
+
+
+/// Handles an attempt in either bouldering or ropes
+/// - Note: Speed not handled yet
+public struct Attempt: Decodable, Hashable, Comparable {
+    private enum BoulderAttempt: String {
+        case top = "T"
+        case zone = "Z"
+        case lowZone = "L"
+        case attempt = "0"
+    }
+    
+    public static func < (lhs: Attempt, rhs: Attempt) -> Bool {
+        if lhs.score == "0", rhs.score == "0" {
+            return lhs.attempt > rhs.attempt
+        }
+        return lhs.value < rhs.value
+    }
+    
+    public var value: Double {
+        
+        switch discipline {
+        case .leadTR:
+            var localScore: Double = 0.0
+            if score.lowercased() == "top" {
+                localScore = 100
+            } else if score.contains("+") {
+                let strippedScore = score.replacingOccurrences(of: "+", with: "")
+                let rawValue = Double(strippedScore) ?? 0.0
+                localScore = rawValue + 0.5
+            } else {
+                localScore = Double(score) ?? 0.0
+            }
+            
+            localScore -= (Double(attempt) * 0.001)
+            return localScore
+        case .boulder:
+            guard let boulderAttempt = BoulderAttempt(rawValue: self.score) else { return 0.0 }
+            switch boulderAttempt {
+            case .top:
+                return Double(100_000_000 - (attempt * 100_000))
+            case .zone:
+                return Double(100_000 - (attempt * 100))
+            case .lowZone:
+                return Double(100 - attempt)
+            case .attempt:
+                return 0
+            }
+        case .speed:
+            return 0.0
+        }
+    }
+    
+    public let discipline: Discipline
+    public let score: String
+    public let attempt: Int
+    
+    init(discipline: Discipline, score: String, attempt: Int) {
+        self.discipline = discipline
+        self.score = score
+        self.attempt = attempt
+    }
+}
--- Sources/USACResultsService/Models/Events/RouteCards/YouthRouteCard.swift
+++ Sources/USACResultsService/Models/Events/RouteCards/YouthRouteCard.swift
@@ -19,11 +19,29 @@ public struct YouthRouteCard: Decodable {
         case round
     }
     
-    public let attempts: [String]
+    public let attempts: [Attempt]
     public let discipline: Discipline
     public let eventId: String
     public let laneId: String
     public let memberId: String
     public let routeId: String
     public let round: EventRound
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.discipline = try container.decode(Discipline.self, forKey: .discipline)
+        self.eventId = try container.decode(String.self, forKey: .eventId)
+        self.laneId = try container.decode(String.self, forKey: .laneId)
+        self.memberId = try container.decode(String.self, forKey: .memberId)
+        self.routeId = try container.decode(String.self, forKey: .routeId)
+        self.round = try container.decode(EventRound.self, forKey: .round)
+        
+        /// Attempts need to be crafted individually
+        let rawAttempts = try container.decode([String].self, forKey: .attempts)
+        var builtAttempts: [Attempt] = []
+        for (index, element) in rawAttempts.enumerated() {
+            builtAttempts.append(Attempt(discipline: discipline, score: element, attempt: index + 1))
+        }
+        self.attempts = builtAttempts
+    }
 }
